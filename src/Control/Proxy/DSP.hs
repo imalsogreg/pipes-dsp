6bab475a381aad8461288993e05a038b4ee3a4b6
Greg Hale <imalsogreg@gmail.com>
2013-07-25 00:29:53 -0400

    Added pipe to take irregularly length vectors and output regularly lengthed ones, and a function to generate iir filter pipes from lists of biquad coefficients

--- src/Control/Proxy/DSP.hs
+++ src/Control/Proxy/DSP.hs
@@ -1,6 +1,7 @@
 module Control.Proxy.DSP where
 
 import Control.Proxy
+import qualified Data.Vector.Unboxed as V
 
 -- |Biquad filter pipe
 -- y0 = (b0 x0 + b1 x1 + b2 x2 - a1 y1 - a2 y2) / g
@@ -27,12 +28,12 @@ firstOrderP :: (Proxy p, Floating x, Monad m)
                -> x -- ^ a1 (n-1  output's coefficient)
                -> x -- ^ filter gain
                -> () -> Pipe p x x m r
-firstOrderP b0 b1 a1 g () = runIdentitpP $ loop 0 0
+firstOrderP b0 b1 a1 g () = runIdentityP $ loop 0 0
   where
     gInv = 1 / g
     loop x1 y1 = do
       x0 <- request ()
-      let y0 = (b0*x0 + b1*x1 - a1*y1) * gInf
+      let y0 = (b0*x0 + b1*x1 - a1*y1) * gInv
       respond y0
       loop x0 y0
 
@@ -41,6 +42,41 @@ firstOrderP b0 b1 a1 g () = runIdentitpP $ loop 0 0
 allPassSectionP :: (Proxy p, Floating x, Monad m) => () -> Pipe p x x m r
 allPassSectionP = biquadDirect1P 1 0 0 0 0 1
 
-iirP :: 
+-- |Construct an iir filter from a series of
+-- second-order sections
+iirP :: (Proxy p, Floating x, Monad m)
+        => [(x, x, x, x, x, x)] -- ^ list of biquad params (b0,b1,b2,a1,a2,g)
+        -> () -> Pipe p x x m r
+iirP biquadParams = foldr (\a b -> (a >-> b)) pull biquads
+  where
+    biquads = map
+              (\(b0, b1, b2, a1, a2, g) -> biquadDirect1P b0 b1 b2 a1 a2 g)
+              biquadParams
 
---firP :: (Proxy p) 
+myBiquad :: (Proxy p, Monad m) => () -> Pipe p Double Double m r
+myBiquad = biquadDirect1P 1 0 0 1 1 1
+
+myIIR :: (Proxy p, Monad m) => () -> Pipe p Double Double m r
+myIIR = iirP [(1, 0, 0, 1, 1, 1),(1, 0, 0, 1, 1, 1)]
+--firP :: (Proxy p)
+
+sputterTestData :: [V.Vector Double]
+sputterTestData = [ V.fromList [1,2]
+                  , V.fromList [3,4,5,6,7,8]
+                  , V.fromList [9]
+                  , V.fromList [10, 11, 12]
+                  ]
+
+unSputter :: (Proxy p, Floating x, V.Unbox x, Monad m)
+             => Int -> () -> Pipe p (V.Vector x) (V.Vector x) m r
+unSputter chunkSize () = runIdentityP $ loop (V.empty, 0)
+  where
+    loop (buffer, bufferLen)
+      | bufferLen >= chunkSize = do
+        let toSend  = V.take chunkSize buffer
+            theRest = V.drop chunkSize buffer
+        respond toSend
+        loop (theRest, bufferLen - chunkSize)
+      | otherwise = do
+        newBuffer <- request ()
+        loop (buffer V.++ newBuffer, bufferLen + V.length newBuffer)
